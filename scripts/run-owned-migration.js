439d47e17ed96ba655ed190b6ec0b02db374e239
Nikichh1 <chavdarov08@gmail.com>
2026-09-20 01:08:35 +0300

    Remove temporary migration runner

--- scripts/run-owned-migration.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const migration = require("../api/migrate-owned");
-migration.runOwnershipMigration().catch(function (error) {
-  console.error(error && error.stack || error);
-  process.exitCode = 1;
-});
